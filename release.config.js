d89c5dad72458cdfe7e9bdb50079333434f79b07
jthomason22 <jthomason@phdata.io>
2022-04-29 12:11:05 -0500

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    branches: "master",
+    repositoryURL: "https://github.com/jthomason22/react-app",
+    plugins: [
+        '@semantic-release/commit-analyzer', 
+        '@semantic-release/release-notes-generator', 
+        '@semantic-release/github'
+    ]
+}
