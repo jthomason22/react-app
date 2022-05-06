c08b084a2928e9addd9cbd9b98ede7fc1671c571
jthomason22 <jthomason@phdata.io>
2022-05-06 14:29:25 -0500

    fix: fixed name variable in index.js and rebuilt

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -8765,7 +8765,7 @@ const github = __nccwpck_require__(3134);
 try {
     // throw new Error("some error message");
     const name = core.getInput("who-to-greet");
-    console.log("Hello ${name}");
+    console.log("Hello ${{ name }}");
 
     const time = new Date();
     core.setOutput("time", time.toTimeString());
