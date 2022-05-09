6230051e9d1e914061ef61b5f74de04436d1a0c9
jthomason22 <jthomason@phdata.io>
2022-05-09 15:22:49 -0500

    test: Update index.js

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -8764,16 +8764,27 @@ const github = __nccwpck_require__(3134);
 
 try {
     //throw new Error("some error message");
+
+    core.debug('Debug message');
+    core.warning('Warning message');
+    core.error('Warning message');
+
     const nameToGreet = core.getInput("who-to-greet");
+    core.setSecret(nameToGreet);
     console.log(`Hello ${nameToGreet}`);
 
     const time = new Date();
     core.setOutput("time", time.toTimeString());
 
+    core.startGroup('Logging github ortecy')
     console.log(JSON.stringify(github, null, "\t"));
-} catch (error) {
-    core.setFailed(error.message);
-}
+    core.endGroup();
+    screenTop
+
+    core.exportVariable("(HELLO", "Hellos");
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 
 })();
 
