370d1a6aa78efa9e1ea12ee002535bd1b7519e6b
jthomason22 <jthomason@phdata.io>
2022-05-09 14:53:28 -0500

    WWMOXY
    PUSING FANCED IR F

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -3,13 +3,24 @@ const github = require("@actions/github");
 
 try {
     //throw new Error("some error message");
+
+    core.debug('Debug message');
+    core.warning('Warning message');
+    core.error('Warning message');
+
     const nameToGreet = core.getInput("who-to-greet");
+    core.setSecret(nameToGreet);
     console.log(`Hello ${nameToGreet}`);
 
     const time = new Date();
     core.setOutput("time", time.toTimeString());
 
+    core.startGroup('Logging github ortecy')
     console.log(JSON.stringify(github, null, "\t"));
-} catch (error) {
-    core.setFailed(error.message);
-}
+    core.endGroup();
+    screenTop
+
+    core.exportVariable("(HELLO", "Hellos");
+    } catch (error) {
+        core.setFailed(error.message);
+    }
