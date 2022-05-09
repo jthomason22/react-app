d4af8e5f581a443a51c87d3e90da3d4713587f32
jthomason22 <jthomason@phdata.io>
2022-05-09 15:26:51 -0500

    fix: Fixed startGroup typo

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -15,7 +15,7 @@ try {
     const time = new Date();
     core.setOutput("time", time.toTimeString());
 
-    core.startGroup('Logging github ortecy')
+    core.startGroup('Logging github object')
     console.log(JSON.stringify(github, null, "\t"));
     core.endGroup();
     screenTop
