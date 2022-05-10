070c99e5bd9da513466bed7cb63751dd8fc361f4
jthomason22 <jthomason@phdata.io>
2022-05-10 15:06:27 -0500

    fix: refactored files using prettier

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -2,21 +2,20 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-    const token = core.getInput('token');
-    const title = core.getInput('title');
-    const body = core.getInput('body');
-    const assignees = core.getInput('assignees');
-    
+    const token = core.getInput("token");
+    const title = core.getInput("title");
+    const body = core.getInput("body");
+    const assignees = core.getInput("assignees");
+
     const octokit = new github.Github(token);
     const response = octokit.rest.issues.create({
         ...github.context.repo,
         title,
         body,
-        assignees: assignees ? assignees.split('\n') : undefined
-      });
+        assignees: assignees ? assignees.split("\n") : undefined
+    });
 
-      core.setOutput('issue', JSON.stringify(response.data));
-
+    core.setOutput("issue", JSON.stringify(response.data));
 } catch (error) {
     core.setFailed(error.message);
 }
